8a41b89aff3cdd9964e4f77ba2425d5cfe1ee0ac
younggeun <yoon125@naver.com>
2021-06-16 15:17:34 +0900

    First Java

---
+++ firstProject/src/firstProject/com/test/Hello.java
@@ -0,0 +1,11 @@
+package firstProject.com.test;
+
+public class Hello {
+	/*	
+	 * method : 기능
+		main method : 프로그램의 진입점 (처음으로 실행)  
+	 */	
+	public static void main(String[] args) {
+		System.out.println("Hello firstProject");
+	}
+}
