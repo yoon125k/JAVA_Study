43d812a52f12184ee90e7df9a5844a022c1a431c
younggeun <yoon125@naver.com>
2021-06-22 09:14:25 +0900

    .

--- src/com/test01/Iftest03.java
+++ src/com/test01/Iftest03.java
@@ -54,19 +54,19 @@ public class Iftest03 {
 					System.out.println("A등급");
 				}
 
-			} else if (80 <= score && score < 90) {
+			} else if (80 <= score) {
 				if (85 <= score) {
 					System.out.println("B+등급");
 				} else {
 					System.out.println("B등급");
 				}
-			} else if (70 <= score && score < 80) {
+			} else if (70 <= score) {
 				if (75 <= score) {
 					System.out.println("C+등급");
 				}else {
 					System.out.println("C등급");
 				}
-			} else if (60 <= score && score < 70) {
+			} else if (60 <= score) {
 				if(65 <= score) {
 					System.out.println("D+등급");
 				}else {
