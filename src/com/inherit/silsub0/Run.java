950a74ff6808db22e711dcca1cb4e787fa13704c
younggeun <yoon125@naver.com>
2021-07-06 12:00:59 +0900

    .

--- src/com/inherit/silsub0/Run.java
+++ src/com/inherit/silsub0/Run.java
@@ -3,7 +3,12 @@ package com.inherit.silsub0;
 public class Run {
 
 	public static void main(String[] args) {
-
+		Car c1 = new SportsCar("노란");
+		c1.accelPedal();
+		System.out.println(c1);
+		c1.accelPedal();
+		c1.accelPedal();
+		System.out.println(c1);
 	}
 
 }
